90f1f264304294e5c3f943bb9f3287104c4bce8c
supreme96 <sahiltandon1996@gmail.com>
2016-07-06 14:36:49 +0000

    App Theme Defined

---
+++ app/src/main/java/com/s4plabs/logbook/ui/Editor.java
@@ -0,0 +1,16 @@
+package com.s4plabs.logbook.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by sahil on 06-Jul-16.
+ */
+public class Editor extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
